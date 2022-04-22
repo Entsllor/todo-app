ff54d2364ecfb5b14164d3c05c995fedd390c86a
Entsllor <slava90100@yandex.com>
2022-04-22 15:50:30 +0500

    feat: configure alembic

--- backend/src/app.py
+++ backend/src/app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 from .core.settings import settings
+from .core.database import db, migrate
 
 
 def create_app():
@@ -9,12 +10,12 @@ def create_app():
     new_app.config["SECRET_KEY"] = settings.SECRET_KEY
     new_app.config["SQLALCHEMY_DATABASE_URI"] = settings.DB_URI
     new_app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
+    db.init_app(new_app)
+    migrate.init_app(new_app, db, directory=settings.ALEMBIC_PATH)
     return new_app
 
 
 app = create_app()
-db = SQLAlchemy(app=app)
-
 
 if __name__ == '__main__':
     app.run(host=settings.HOST, port=settings.PORT, debug=settings.DEBUG)
--- backend/src/core/settings.py
+++ backend/src/core/settings.py
@@ -1,7 +1,7 @@
 from pydantic import BaseSettings
 from pathlib import Path
 
-BASE_PATH = Path(__file__)
+BASE_PATH = Path(__file__).parent.parent
 
 
 class AppSettings(BaseSettings):
@@ -10,6 +10,7 @@ class AppSettings(BaseSettings):
     JWT_ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_SECONDS: int = 60 * 20
     REFRESH_TOKEN_EXPIRE_SECONDS: int = 60 * 60 * 24 * 30
+    ALEMBIC_PATH: Path | str = BASE_PATH.parent.joinpath("migrations")
 
     class Config:
         env_file = BASE_PATH.joinpath(".env")
